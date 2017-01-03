ab8c586229e00b6c846fbbe828c9c2faca45e2e7
Anderson Pereira <anderson.rasec@outlook.com>
2017-01-03 18:52:25 -0300

    Class 2 Create the folders Conctracts and Repositories. Creata the class IUserRepository.

---
+++ SpaUserControl.Domain/Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.Domain.Contracts.Repositories
+{
+    class IUserRepository
+    {
+    }
+}
