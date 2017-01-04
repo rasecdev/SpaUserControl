26bd7c33113bc3cd89c481049c989f18f71c0ff8
Anderson Pereira <anderson.rasec@outlook.com>
2017-01-03 21:17:22 -0300

    Class 2 Created and implemented the class UserMap.

---
+++ SpaUserControl.Infraestructure/Data/Map/UserMap.cs
@@ -0,0 +1,38 @@
+﻿using SpaUserControl.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.Infraestructure.Data.Map
+{
+    public class UserMap :EntityTypeConfiguration<User>
+    {
+        public UserMap()
+        {
+            ToTable("User");
+
+            //Pelo fato do Id ser um Guid será usado o DatabaseGeneratedOption. 
+            Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Name)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            Property(x => x.Email)
+                .HasMaxLength(160)
+                .HasColumnAnnotation(
+                    IndexAnnotation.AnnotationName,
+                    new IndexAnnotation(new IndexAttribute("IX_EMAIL", 1) { IsUnique = true }))
+                .IsRequired();
+
+            Property(x => x.Password)
+                .HasMaxLength(32)
+                .IsFixedLength(); //Encriptada te`rá 32 caracteres independente do tamnanho da senha.
+        }
+    }
+}
