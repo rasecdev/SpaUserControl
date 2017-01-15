988727d6e932fec0e7f73ce53e4bc1b4f16c35e1
Anderson Pereira <anderson.rasec@outlook.com>
2017-01-14 22:45:10 -0300

    Class 4 Impemented the class IUserDispose.

--- SpaUserControl.Domain/Contracts/Service/IUserService.cs
+++ SpaUserControl.Domain/Contracts/Service/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpaUserControl.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace SpaUserControl.Domain.Contracts.Service
 {
-    public interface IUserService
+    public interface IUserService : IDisposable
     {
+        User Authenticate(string email, string password);
+
+        User GetByEmail(string email);
+
+        void Register(string name, string email, string password, string confirmPassword);
+
+        void ChangeInformation(string email, string name);
+
+        void ChangePassword(string email, string password, string newPassword, string confirmPassword);
+
+        string resetPassword(string email);
     }
 }
