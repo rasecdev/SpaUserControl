6a4a9108bc39da27a7872e9e4bffd68f29075031
Anderson Pereira <anderson.rasec@outlook.com>
2017-01-16 23:50:21 -0300

    Class 6 Created the class StartUp. Installed Unity for dependency.

---
+++ SpaUserControl.Api/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SpaUserControl.Api
+{
+    public class StartUp
+    {
+        //Método obrigatório do Owin.
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureWebApi(config);
+
+            //A aplicação é pública AllowAll.
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        //COnfiguração obrigatória (rotas).
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+             );
+
+    }
+}
